339f2d7782e3acf62fe5023d5cfe1e040acf71ff
Sean Cesmat <sean.cesmat@indigoslate.com>
2020-02-24 09:21:18 -0800

    added more controlls

--- src/App.vue
+++ src/App.vue
@@ -1,65 +1,97 @@
 <template>
     <div id="app">
-        <div class="row">
-            <Card3DFull :bg-image="mountain"
-                :depth-map="mountainDepth"
-                :sensitivity="25">
-                <h1 slot="header">Mountains</h1>
+        <div id="nav">
+
+            <div>
+                Image:
+                <button @click="setImage('subway')"
+                    :class="{active: currentBGName === 'subway'}">Subway</button>
+                <button @click="setImage('mountain')"
+                    :class="{active: currentBGName === 'mountain'}">Mountain</button>
+                <button @click="setImage('boarder')"
+                    :class="{active: currentBGName === 'boarder'}">Boarder</button>
+                <button @click="setImage('controller')"
+                    :class="{active: currentBGName === 'controller'}">Controller</button>
+                <button @click="setImage('shelf')"
+                    :class="{active: currentBGName === 'shelf'}">Shelf</button>
+            </div>
+            <div>
+
+                <strong>Border-Radius:</strong>
+                <input type="text"
+                    v-model="borderRadius"
+                    style="max-width: 30px; margin-left: 10px">
+            </div>
+            <div>
+                <strong>Sensitivity:</strong>
+                <input type="text"
+                    v-model="sensitivity"
+                    style="max-width: 30px; margin-left: 10px">
+                <span style="font-size: 12px; padding-left: 5px">(lower number = more
+                    sensitive)</span>
+                <span style="font-size: 12px; padding-left: 5px">Recommended for this image:
+                    <strong style="color: #3bef9d;">{{ recommendedSensitivity }}</strong></span>
+            </div>
+
+        </div>
+        <div class="row"
+            style="margin-top: 140px;">
+            <Card3DFull :key="`Card3DFull-${currentBGName}`"
+                :bg-image="currentBG"
+                :depth-map="currentBGDepth"
+                :border-radius="borderRadius"
+                :sensitivity="sensitivity">
+                <h1 slot="header">{{ currentBGName }}</h1>
                 <p slot="content">Lorem ipsum dolor sit amet, consectetur adipisicing.</p>
             </Card3DFull>
-            <TippyCardFull :bg-image="subway"
-                :depth-map="subwayDepth"
-                :sensitivity="25">
-                <h1 slot="header">Subway</h1>
+            <TippyCardFull :key="`TippyCardFull-${currentBGName}`"
+                :bg-image="currentBG"
+                :depth-map="currentBGDepth"
+                :border-radius="borderRadius"
+                :sensitivity="sensitivity">
+                <h1 slot="header">{{ currentBGName }}</h1>
                 <p slot="content">Lorem ipsum dolor sit amet, consectetur adipisicing.</p>
             </TippyCardFull>
         </div>
+        <h3 style="margin-top: 100px">With a persistant info box</h3>
         <div class="row">
-            <Card3DFull :bg-image="mountain"
-                :depth-map="mountainDepth"
-                :border-radius="10"
-                :sensitivity="25">
-                <h1 slot="header">Mountains</h1>
-                <p slot="content">Lorem ipsum dolor sit amet, consectetur adipisicing.</p>
-            </Card3DFull>
-            <TippyCardFull :bg-image="subway"
-                :depth-map="subwayDepth"
-                :border-radius="10"
-                :sensitivity="25">
-                <h1 slot="header">Subway</h1>
-                <p slot="content">Lorem ipsum dolor sit amet, consectetur adipisicing.</p>
-            </TippyCardFull>
-        </div>
-        <h3 style="margin-top: 100px">The following are the same just different images and
-            border-radius</h3>
-        <div class="row">
-            <Card3D :bg-image="boarder"
-                :depth-map="boarderDepth"
-                :sensitivity="60">
-                <h1 slot="header">Snowboarder</h1>
+            <Card3D :key="`Card3D-A-${currentBGName}`"
+                :bg-image="currentBG"
+                :depth-map="currentBGDepth"
+                :border-radius="borderRadius"
+                :sensitivity="sensitivity">
+                <h1 slot="header">{{ currentBGName }}</h1>
                 <p slot="content">Lorem ipsum dolor sit amet, consectetur adipisicing.</p>
             </Card3D>
-            <TippyCard :bg-image="shelf"
-                :depth-map="shelfDepth"
-                :sensitivity="40"
-                xymove>
-                <h1 slot="header">Shelf</h1>
+            <TippyCard :key="`TippyCard-A-${currentBGName}`"
+                :bg-image="currentBG"
+                :depth-map="currentBGDepth"
+                :border-radius="borderRadius"
+                :sensitivity="sensitivity">
+                <h1 slot="header">{{ currentBGName }}</h1>
                 <p slot="content">Lorem ipsum dolor sit amet, consectetur adipisicing.</p>
             </TippyCard>
         </div>
-        <div class="row">
-            <Card3D :bg-image="subway"
-                :depth-map="subwayDepth"
-                :border-radius="10"
-                :sensitivity="35">
-                <h1 slot="header">Subway</h1>
+        <h3 style="margin: 100px auto 0px; max-width: 752px;">With a popup info box</h3>
+        <div class="row"
+            style="margin-bottom: 100px;">
+            <Card3D :key="`Card3D-C-${currentBGName}`"
+                :bg-image="currentBG"
+                :depth-map="currentBGDepth"
+                :border-radius="borderRadius"
+                :sensitivity="sensitivity"
+                info-popup>
+                <h1 slot="header">{{ currentBGName }}</h1>
                 <p slot="content">Lorem ipsum dolor sit amet, consectetur adipisicing.</p>
             </Card3D>
-            <TippyCard :bg-image="controller"
-                :depth-map="controllerDepth"
-                :border-radius="10"
-                :sensitivity="40">
-                <h1 slot="header">Games</h1>
+            <TippyCard :key="`TippyCard-C-${currentBGName}`"
+                :bg-image="currentBG"
+                :depth-map="currentBGDepth"
+                :border-radius="borderRadius"
+                :sensitivity="sensitivity"
+                xymove
+                info-popup>
+                <h1 slot="header">{{ currentBGName }}</h1>
                 <p slot="content">Lorem ipsum dolor sit amet, consectetur adipisicing.</p>
             </TippyCard>
         </div>
@@ -72,91 +104,21 @@
                 shifting</h2>
         </div>
         <div class="row">
-            <TippyCard :bg-image="subway"
-                :depth-map="subwayDepth"
-                :border-radius="6"
-                :sensitivity="30">
-                <h1 slot="header">Subway</h1>
+            <TippyCard :key="`TippyCard-B-${currentBGName}`"
+                :bg-image="currentBG"
+                :depth-map="currentBGDepth"
+                :border-radius="borderRadius"
+                :sensitivity="sensitivity">
+                <h1 slot="header">{{ currentBGName }}</h1>
                 <p slot="content">Lorem ipsum dolor sit amet, consectetur adipisicing.</p>
             </TippyCard>
-            <TippyCard :bg-image="subway"
-                :depth-map="subwayDepth"
-                :border-radius="6"
-                :sensitivity="30"
+            <TippyCard :key="`TippyCardXYMove-${currentBGName}`"
+                :bg-image="currentBG"
+                :depth-map="currentBGDepth"
+                :border-radius="borderRadius"
+                :sensitivity="sensitivity"
                 xymove>
-                <h1 slot="header">Subway</h1>
-                <p slot="content">Lorem ipsum dolor sit amet, consectetur adipisicing.</p>
-            </TippyCard>
-        </div>
-        <div class="row">
-            <TippyCard :bg-image="mountain"
-                :depth-map="mountainDepth"
-                :border-radius="6"
-                :sensitivity="30">
-                <h1 slot="header">Mountain</h1>
-                <p slot="content">Lorem ipsum dolor sit amet, consectetur adipisicing.</p>
-            </TippyCard>
-            <TippyCard :bg-image="mountain"
-                :depth-map="mountainDepth"
-                :border-radius="6"
-                :sensitivity="30"
-                xymove>
-                <h1 slot="header">Mountain</h1>
-                <p slot="content">Lorem ipsum dolor sit amet, consectetur adipisicing.</p>
-            </TippyCard>
-        </div>
-        <!-- <div class="row">
-            <TippyCard :bg-image="flowers"
-                :depth-map="flowersDepth"
-                :border-radius="6"
-                :sensitivity="30">
-                <h1 slot="header">Flowers</h1>
-                <p slot="content">Lorem ipsum dolor sit amet, consectetur adipisicing.</p>
-            </TippyCard>
-            <TippyCard :bg-image="flowers"
-                :depth-map="flowersDepth"
-                :border-radius="6"
-                :sensitivity="30"
-                xymove>
-                <h1 slot="header">Flowers</h1>
-                <p slot="content">Lorem ipsum dolor sit amet, consectetur adipisicing.</p>
-            </TippyCard>
-        </div> -->
-        <!-- <div class="row">
-            <TippyCard :bg-image="boarder"
-                :depth-map="boarderDepth"
-                :border-radius="6"
-                :sensitivity="50">
-                <h1 slot="header">Boarder</h1>
-                <p slot="content">Lorem ipsum dolor sit amet, consectetur adipisicing.</p>
-            </TippyCard>
-            <TippyCard :bg-image="boarder"
-                :depth-map="boarderDepth"
-                :border-radius="6"
-                :sensitivity="50"
-                xymove>
-                <h1 slot="header">Boarder</h1>
-                <p slot="content">Lorem ipsum dolor sit amet, consectetur adipisicing.</p>
-            </TippyCard>
-        </div> -->
-        <h3 style="margin: 100px auto 0px; max-width: 752px;">The following is to show a version
-            with a popup info box</h3>
-        <div class="row">
-            <Card3D :bg-image="boarder"
-                :depth-map="boarderDepth"
-                :border-radius="6"
-                :sensitivity="50"
-                info-popup>
-                <h1 slot="header">Boarder</h1>
-                <p slot="content">Lorem ipsum dolor sit amet, consectetur adipisicing.</p>
-            </Card3D>
-            <TippyCard :bg-image="boarder"
-                :depth-map="boarderDepth"
-                :border-radius="6"
-                :sensitivity="50"
-                xymove
-                info-popup>
-                <h1 slot="header">Boarder</h1>
+                <h1 slot="header">{{ currentBGName }}</h1>
                 <p slot="content">Lorem ipsum dolor sit amet, consectetur adipisicing.</p>
             </TippyCard>
         </div>
@@ -202,13 +164,53 @@ export default {
             shelf,
             shelfDepth,
             flowers,
-            flowersDepth
+            flowersDepth,
+            currentBG: subway,
+            currentBGDepth: subwayDepth,
+            currentBGName: 'subway',
+            borderRadius: 10,
+            sensitivity: 30,
+            recommendedSensitivity: 30
         };
+    },
+    methods: {
+        setImage(name) {
+            if (name === 'subway') {
+                this.currentBG = subway;
+                this.currentBGDepth = subwayDepth;
+                this.currentBGName = 'subway';
+                this.recommendedSensitivity = 30;
+            }
+            if (name === 'mountain') {
+                this.currentBG = mountain;
+                this.currentBGDepth = mountainDepth;
+                this.currentBGName = 'mountain';
+                this.recommendedSensitivity = 45;
+            }
+            if (name === 'boarder') {
+                this.currentBG = boarder;
+                this.currentBGDepth = boarderDepth;
+                this.currentBGName = 'boarder';
+                this.recommendedSensitivity = 60;
+            }
+            if (name === 'controller') {
+                this.currentBG = controller;
+                this.currentBGDepth = controllerDepth;
+                this.currentBGName = 'controller';
+                this.recommendedSensitivity = 45;
+            }
+            if (name === 'shelf') {
+                this.currentBG = shelf;
+                this.currentBGDepth = shelfDepth;
+                this.currentBGName = 'shelf';
+                this.recommendedSensitivity = 50;
+            }
+        }
     }
 };
 </script>
 
-<style>
+<style lang="scss">
 #app {
     font-family: "Avenir", Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
@@ -222,8 +224,58 @@ export default {
     margin: 0 auto;
     display: flex;
 }
-
+h1 {
+    text-transform: capitalize;
+}
 h3 {
     color: #41b3a3;
+}
+
+#nav {
+    position: fixed;
+    width: 100%;
+    z-index: 100;
+    top: 0;
+    left: 0;
+    display: flex;
+    justify-content: space-around;
+    padding: 16px 30px;
+    background: rgba(0, 0, 0, 0.75);
+    backdrop-filter: blur(10px);
+    color: #fff;
+    a {
+        font-weight: bold;
+        color: #fff;
+
+        &.router-link-exact-active {
+            color: #3bef9d;
+        }
+    }
+    .active {
+        background: #3bef9d;
+    }
+    button {
+        padding: 5px 10px;
+        display: inline-block;
+        border-radius: 3px;
+        border: none;
+        background: #ccc;
+        font-size: 12px;
+        font-weight: bold;
+        color: #333;
+        margin-left: 4px;
+        margin-right: 4px;
+        cursor: pointer;
+        &:hover {
+            background: #54f7ae;
+        }
+    }
+    input[type="text"] {
+        background: #3bef9d;
+        font-weight: bold;
+        font-size: 14px;
+        border: none;
+        text-align: center;
+    }
 }
 </style>
